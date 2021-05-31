7a1c1bb0ad42c7d1b5ea9eeae4e7cc98c643254f
subalaxmi-99 <84506730+subalaxmi-99@users.noreply.github.com>
2021-05-31 11:28:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -11,16 +11,7 @@ public static void main( String[] args )
 {
 
  
-Scanner sc= new Scanner(System.in); //System.in is a standard input stream
-
- 
-System.out.print("Enter a string: ");
-
- 
-String str= sc.nextLine(); //reads string
-
- 
-System.out.print("You have entered: "+str);
+System.out.println(args[0]);
 
  
 }
